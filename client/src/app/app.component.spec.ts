68653f17b8bf89f2b2201dcbc7ec5ff02e90a461
SychovSergei <sychov.sergei@gmail.com>
2024-01-25 01:31:05 +0200

    change main layout
    delete default project and add some container and styles

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,31 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {PasswordModule} from "./common/components/password/password.module";
+import {PasswordTdFormModule} from "./common/components/password-td-form/password-td-form.module";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+      imports: [
+        PasswordModule,
+        PasswordTdFormModule,
+      ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
   });
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title', () => {
+    const titleElement: HTMLElement = fixture.nativeElement.querySelector('h2');
+    expect(titleElement.textContent).toContain('Password strength');
+  });
+
 });
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { PasswordModule } from "./common/components/password/password.module";
+import { PasswordTdFormModule } from "./common/components/password-td-form/password-td-form.module";
+import { UtilsModule } from "./common/utils/utils.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // PasswordModule,
+    // PasswordTdFormModule,
+    // UtilsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
